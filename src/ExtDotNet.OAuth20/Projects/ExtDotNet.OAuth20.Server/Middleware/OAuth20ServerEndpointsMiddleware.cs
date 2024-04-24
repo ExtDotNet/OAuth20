8857a75b5f87384a80e1d3483430d6f996de2754
LefeueteLaverite <lefeuetelaverite@gmail.com>
2024-04-25 01:47:10 +0300

    [#2] add middleware

---
+++ src/ExtDotNet.OAuth20/Projects/ExtDotNet.OAuth20.Server/Middleware/OAuth20ServerEndpointsMiddleware.cs
@@ -0,0 +1,53 @@
+﻿// Developed and maintained by ExtDotNet.
+// ExtDotNet licenses this file to you under the MIT license.
+
+using ExtDotNet.OAuth20.Server.Abstractions;
+using ExtDotNet.OAuth20.Server.Abstractions.Endpoints;
+using ExtDotNet.OAuth20.Server.Abstractions.Errors;
+using ExtDotNet.OAuth20.Server.Abstractions.Errors.Exceptions;
+
+namespace ExtDotNet.OAuth20.Server.Middleware;
+
+public class OAuth20ServerEndpointsMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public OAuth20ServerEndpointsMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext, IEndpointRouter router, ITlsValidator tlsValidator, IErrorResultProvider errorResultProvider)
+    {
+        if (router.TryGetEndpoint(httpContext, out IEndpoint? endpoint))
+        {
+            var validationResult = tlsValidator.TryValidate(httpContext);
+
+            if (!validationResult.Success)
+            {
+                throw new InvalidOperationException(validationResult.Description);
+            }
+
+            IResult result;
+
+            try
+            {
+                result = await endpoint!.InvokeAsync(httpContext);
+            }
+            catch (OAuth20Exception exception)
+            {
+                result = errorResultProvider.GetErrorResult(exception);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            await result.ExecuteAsync(httpContext);
+        }
+        else
+        {
+            await _next.Invoke(httpContext);
+        }
+    }
+}
---
+++ src/ExtDotNet.OAuth20/Projects/ExtDotNet.OAuth20.Server/Middleware/OAuth20ServerWebPagesMiddleware.cs
@@ -0,0 +1,53 @@
+﻿// Developed and maintained by ExtDotNet.
+// ExtDotNet licenses this file to you under the MIT license.
+
+using ExtDotNet.OAuth20.Server.Abstractions.Errors.Exceptions;
+using ExtDotNet.OAuth20.Server.Abstractions.Errors;
+using ExtDotNet.OAuth20.Server.Abstractions;
+using ExtDotNet.OAuth20.Server.Abstractions.WebPageBuilders;
+
+namespace ExtDotNet.OAuth20.Server.Middleware;
+
+public class OAuth20ServerWebPagesMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public OAuth20ServerWebPagesMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext, IWebPageBuilderRouter router, ITlsValidator tlsValidator, IErrorResultProvider errorResultProvider)
+    {
+        if (router.TryGetWebPageBuilder(httpContext, out IWebPageBuilder? webPageBuilder))
+        {
+            var validationResult = tlsValidator.TryValidate(httpContext);
+
+            if (!validationResult.Success)
+            {
+                throw new InvalidOperationException(validationResult.Description);
+            }
+
+            IResult result;
+
+            try
+            {
+                result = await webPageBuilder!.InvokeAsync(httpContext);
+            }
+            catch (OAuth20Exception exception)
+            {
+                result = errorResultProvider.GetErrorResult(exception);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            await result.ExecuteAsync(httpContext);
+        }
+        else
+        {
+            await _next.Invoke(httpContext);
+        }
+    }
+}
